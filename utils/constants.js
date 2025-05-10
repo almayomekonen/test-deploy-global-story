4281e81d56eb01f3adc5d9396d8f6a40c76f01df
almayomekonen <alamayo274@gmail.com>
2025-05-10 23:18:12 +0300

    new url-test-7-images

--- utils/constants.js
+++ utils/constants.js
@@ -1,25 +1,15 @@
-// src/utils/constants.js
-
 export const getProfileImageUrl = (profileImage) => {
   if (!profileImage) return "/default-avatar.png";
 
-  if (profileImage && profileImage.startsWith("http")) return profileImage;
+  if (profileImage.startsWith("http")) return profileImage;
 
-  if (window.location.hostname === "localhost") {
-    return `http://localhost:4000/uploads/profiles/${profileImage}`;
-  }
-
-  return `/uploads/profiles/${profileImage}`;
+  return `https://aardvark-stories-api.onrender.com/uploads/profiles/${profileImage}`;
 };
 
 export const getPostImageUrl = (image) => {
   if (!image) return "/placeholder-image.png";
 
-  if (image && image.startsWith("http")) return image;
-
-  if (window.location.hostname === "localhost") {
-    return `http://localhost:4000/uploads/posts/${image}`;
-  }
+  if (image.startsWith("http")) return image;
 
   return `https://aardvark-stories-api.onrender.com/uploads/posts/${image}`;
 };
